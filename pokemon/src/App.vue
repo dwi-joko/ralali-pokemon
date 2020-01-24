86a2c4b8c55f6403bcb9eca3ea531c7a68fa6ec4
dwi joko purnomo <dwijokopurnomo10@gmail.com>
2020-01-24 08:25:51 +0700

    update npm and update code

--- pokemon/src/App.vue
+++ pokemon/src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <img alt="Vue logo" src="./assets/logo.png">
+    <div class="search">
+      <input type=" text" v-model="search" placeholder="pokemon name" />
+      <span class="dropdown">
+      </span>
+
+    </div>
+    <div >
+      <ul>
+        <li v-for="results in filterResults" :key="results" class="tet">
+          {{ info }}
+          <h2>{{ results.name }}</h2>
+
+          <span class="image">
+          </span>
+
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+name: 'App',
+data () {
+return {
+results: [],
+search: ''
 }
+},
+mounted() {
+axios.get("https://pokeapi.co/api/v2/pokemon/")
+.then(response => {this.results = response.data.results})
+},
+computed:{
+filterResults: function(){
+return this.results.filter((results) =>{
+return results.name.match(this.search);
+});
+}
+}} 
 </script>
 
 <style lang="scss">
@@ -24,5 +54,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .search {
+    max-width: 25%;
+    margin: 0 auto;
+    background: #f9f9f9;
+    padding: 10px 20px;
+    text-align: left;
+    input {
+        padding: 10px;
+        border: 1px solid #eaeaea;
+        box-shadow: none !important;
+        font-size: 14px;
+        width: 100%;
+        box-sizing: border-box;
+    }
+  }
+  ul {
+    padding: 0;
+    background: #f9f9f9;
+    li {
+        display: inline-block;
+        padding: 30px;
+        margin: 10px;
+        border: 1px solid #eaeaea;
+    }
+  }
 }
 </style>
